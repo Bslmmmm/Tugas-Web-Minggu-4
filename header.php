125c7c32cccf65a3d5a73c713db3100d8bcf4299
Bslmmmm <fahmi.alkaff29@gmail.com>
2024-09-24 22:05:24 +0700

    first commit

---
+++ header.php
@@ -0,0 +1,38 @@
+<header id="header" class="header d-flex align-items-center sticky-top">
+    <div class="container-fluid position-relative d-flex align-items-center justify-content-between">
+
+      <a href="index.html" class="logo d-flex align-items-center me-auto me-xl-0">
+        <!-- Uncomment the line below if you also wish to use an image logo -->
+        <!-- <img src="assets/img/logo.png" alt=""> -->
+        <h1 class="sitename">HeroBiz</h1>
+        <span>.</span>
+      </a>
+
+      <nav id="navmenu" class="navmenu">
+        <ul>
+          <li><a href="#hero" class="active">Home<br></a></li>
+          <li><a href="#service">Mahasiswa</a></li>
+          <li><a href="#portfolio">Portfolio</a></li>
+          <li class="dropdown"><a href="#"><span>Jurusan</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+            <ul>  
+                  <li><a href="#">Teknik Agama</a></li>
+                  <li><a href="#">Bisnis Digital</a></li>
+                  <li><a href="#">Manajemen Informatika</a></li>
+                  <li><a href="#">Teknik Komputer</a></li>
+                  <li><a href="#">Teknik Informatika</a></li>
+              
+              </li>
+              
+            </ul>
+          </li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+        <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+      </nav>
+
+      <a class="btn-getstarted" href="auth/login.php">Login</a>
+      
+      
+
+    </div>
+  </header>
